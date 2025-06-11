fa7de036d45eada6a3fbd1c1bc337c27dbb5c618
walter <walteranyika@gmail.com>
2025-06-11 08:28:57 +0300

    Cleanup

--- app/src/main/java/com/walter/fireapp/MainActivity.kt
+++ app/src/main/java/com/walter/fireapp/MainActivity.kt
@@ -3,7 +3,6 @@ package com.walter.fireapp
 import android.content.Intent
 import android.icu.util.Calendar
 import android.os.Bundle
-import android.widget.DatePicker
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -18,29 +17,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
-
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.firebase.Firebase
 import com.google.firebase.database.database
 import com.walter.fireapp.ui.theme.FireAppTheme
-import kotlin.text.format
-import kotlin.text.get
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,23 +60,15 @@ fun RegForm(innerPadding: PaddingValues = PaddingValues(0.dp)) {
 
     val context = LocalContext.current
 
-    val calendar = Calendar.getInstance()
-    val year = calendar.get(Calendar.YEAR)
-    val month = calendar.get(Calendar.MONTH)
-    val day = calendar.get(Calendar.DAY_OF_MONTH)
-
-
     when{
         isLoading -> {
             CircularProgressIndicator()
         }
         else -> {
-            Column(
-                modifier = Modifier
+            Column(modifier = Modifier
                     .fillMaxSize()
                     .padding(innerPadding)
-                    .padding(16.dp)
-            )
+                    .padding(16.dp))
             {
                 OutlinedTextField(
                     value = name,
